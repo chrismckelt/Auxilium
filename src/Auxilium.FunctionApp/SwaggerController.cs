d6c3c7dfd3c4795c9619a33e47b569ab1bbe1a00
Richard Hughes <richard.hughes@insight.com>
2021-02-15 11:26:06 +0800

    Adding AZ Functions project
    Scaffolding Azure functions with swagger ui

---
+++ src/Auxilium.FunctionApp/SwaggerController.cs
@@ -0,0 +1,30 @@
+using System.Net.Http;
+using System.Threading.Tasks;
+using AzureFunctions.Extensions.Swashbuckle;
+using AzureFunctions.Extensions.Swashbuckle.Attribute;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+
+namespace Auxilium.FunctionApp
+{
+	public static class SwaggerController
+	{
+		[SwaggerIgnore]
+		[FunctionName("Swagger")]
+		public static Task<HttpResponseMessage> Run(
+			[HttpTrigger(AuthorizationLevel.Function, "get", Route = "Swagger/json")] HttpRequestMessage req,
+			[SwashBuckleClient]ISwashBuckleClient swashBuckleClient)
+		{
+			return Task.FromResult(swashBuckleClient.CreateSwaggerDocumentResponse(req));
+		}
+
+		[SwaggerIgnore]
+		[FunctionName("SwaggerUi")]
+		public static Task<HttpResponseMessage> Run2(
+			[HttpTrigger(AuthorizationLevel.Function, "get", Route = "Swagger/ui")] HttpRequestMessage req,
+			[SwashBuckleClient]ISwashBuckleClient swashBuckleClient)
+		{
+			return Task.FromResult(swashBuckleClient.CreateSwaggerUIResponse(req, "swagger/json"));
+		}
+	}
+}
